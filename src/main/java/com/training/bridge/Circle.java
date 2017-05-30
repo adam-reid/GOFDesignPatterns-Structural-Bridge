dc6ef01241f00ca5dbff5972c87cb499f759b392
Adam Reid <adameastcoast@gmail.com>
2017-05-30 01:12:04 -0400

    Add concrete shape

---
+++ src/main/java/com/training/bridge/Circle.java
@@ -0,0 +1,23 @@
+package com.training.bridge;
+
+/**
+ * Created by adame on 5/30/2017.
+ */
+public class Circle extends Shape {
+
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(int x, int y, int radius, DrawAPI drawAPI) {
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        drawApi.drawCircle(radius,x,y);
+    }
+}
